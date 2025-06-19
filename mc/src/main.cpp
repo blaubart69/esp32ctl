692d14603ac8916b689bd542dccde8b99779d584
bee <bernhard.spindler.75@gmail.com>
2025-06-19 22:31:29 +0200

    more mc

--- mc/src/main.cpp
+++ mc/src/main.cpp
@@ -1,11 +1,6 @@
 #include <Arduino.h>
 
 /*
-*   3 bytes
-*     1: API
-*     2: port
-*     3: value 
-*
 *   Let's try ONE byte.
 *   we have  > 16 GPIO pin NUMBERS on ESP32 super mini 
 *   we have <=  8 commands
@@ -26,28 +21,34 @@ enum HOST_CMD {
 
 enum CLIENT_RESPONSE {
     digitalReadHigh = 1,
-    digitalReadLow = 2
+    digitalReadLow = 2,
+    error = 0xFF
 };
-
 
 typedef struct {
     uint8_t cmd;
     uint8_t pin;
 } host_cmd_t;
-
-host_cmd_t decode_command(const uint8_t encoded_cmd) {
+// ----------------------------------------------------------------------------
+host_cmd_t decode_command(const uint8_t encoded_cmd) 
+// ----------------------------------------------------------------------------
+{
     return host_cmd_t {
          .cmd = (uint8_t)(encoded_cmd >> 5)
         ,.pin = (uint8_t)(encoded_cmd & 0x1F)
     };
 }
-
-uint8_t encode_result(const uint8_t cmd, const uint8_t pin) {
+// ----------------------------------------------------------------------------
+uint8_t encode_result(const uint8_t cmd, const uint8_t pin) 
+// ----------------------------------------------------------------------------    
+{
     return (uint8_t)(cmd << 5) | (uint8_t)( pin & 0x1F);
 }
-
-uint8_t get_client_result(int value) {
-    uint8_t result;
+// ----------------------------------------------------------------------------
+CLIENT_RESPONSE get_client_response(int value) 
+// ----------------------------------------------------------------------------    
+{
+    CLIENT_RESPONSE result;
     if ( value == HIGH ) {
         result = CLIENT_RESPONSE::digitalReadHigh;
     }
@@ -55,34 +56,40 @@ uint8_t get_client_result(int value) {
         result = CLIENT_RESPONSE::digitalReadLow;
     }
     else {
-        result = 0xFF;
+        result = CLIENT_RESPONSE::error;
     }
     return result;
 }
-
-void handle_command(const host_cmd_t cmd) {
-    switch (cmd.cmd)
-    {
-    default:
+// ----------------------------------------------------------------------------
+void handle_digitalRead(const uint8_t pin)
+// ----------------------------------------------------------------------------    
+{
+    uint8_t value           = digitalRead(pin);
+    uint8_t client_result   = get_client_response(value);
+    uint8_t reply           = encode_result(client_result, pin);
+    if ( Serial.write(reply) != 1 ) {
         // problem!
-    break;
-    case HOST_CMD::digitalWriteHigh:       digitalWrite(cmd.pin, HIGH);             break;
-    case HOST_CMD::digitalWriteLow:        digitalWrite(cmd.pin, LOW);              break;
-    case HOST_CMD::pinModeInput:           pinMode     (cmd.pin, INPUT);            break;
-    case HOST_CMD::pinModeInputPullup:     pinMode     (cmd.pin, INPUT_PULLUP);     break;
-    case HOST_CMD::pinModeInputPulldown:   pinMode     (cmd.pin, INPUT_PULLDOWN);   break;
-    case HOST_CMD::digiRead:
-            uint8_t value = digitalRead(cmd.pin);
-            uint8_t client_result = get_client_result(value);
-            uint8_t reply = encode_result(client_result, cmd.pin);
-            if ( Serial.write(reply) != 1 ) {
-                // problem!
-            }
-        break;
     }
 }
-
-void  onRxEvent(
+//----------------------------------------------------------------------------
+bool handle_command(const host_cmd_t cmd)
+//----------------------------------------------------------------------------    
+{   
+    switch (cmd.cmd)
+    {
+        default: return false;
+        case HOST_CMD::digitalWriteHigh:       digitalWrite(cmd.pin, HIGH);                  break;
+        case HOST_CMD::digitalWriteLow:        digitalWrite(cmd.pin, LOW);                   break;
+        case HOST_CMD::pinModeInput:           pinMode     (cmd.pin, INPUT);                 break;
+        case HOST_CMD::pinModeInputPullup:     pinMode     (cmd.pin, INPUT_PULLUP);          break;
+        case HOST_CMD::pinModeInputPulldown:   pinMode     (cmd.pin, INPUT_PULLDOWN);        break;
+        case HOST_CMD::digiRead:               handle_digitalRead(cmd.pin);                  break;
+    }
+    return true;
+}
+//----------------------------------------------------------------------------
+void onRxEvent(
+//----------------------------------------------------------------------------
   void*               event_handler_arg
   ,esp_event_base_t   event_base
   ,int32_t            event_id
@@ -91,21 +98,31 @@ void  onRxEvent(
     int available;
     while ( (available = Serial.available()) != 0 )
     {
-        byte data;
+        uint8_t data;
         if ( Serial.read(&data, 1) != 1 ) {
             // problem!
         }
         else {
             host_cmd_t cmd = decode_command(data);
-            handle_command(cmd);
+            if ( ! handle_command(cmd) ) {
+                Serial.write(CLIENT_RESPONSE::error);
+            } 
+            else if ( cmd.cmd == HOST_CMD::digiRead ) {
+                // do not send an ACK
+            }
+            else {
+                Serial.write(data);      
+            }
         }
     }
 }
-
-void setup() {
-  Serial.begin(230400);
-  pinMode(8,OUTPUT); // build in LED
-  Serial.onEvent( ARDUINO_HW_CDC_RX_EVENT, onRxEvent );
+// ----------------------------------------------------------------------------
+void setup()
+// ----------------------------------------------------------------------------
+{
+    Serial.begin(230400);
+    pinMode(8,OUTPUT); // build in LED
+    Serial.onEvent( ARDUINO_HW_CDC_RX_EVENT, onRxEvent );
 }
 
 // the loop function runs over and over again forever
